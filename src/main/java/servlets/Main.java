ae76fe2494660f0f57fc87e285d8f3396f052907
Матвей Веприцкий <>
2015-12-21 01:20:21 +0300

    Учетные записи пользователей отныне хранятся на удалённой БД. Реализован функционал, позволяющий взаимодействовать с вышеуказанной БД.

--- src/main/java/servlets/Main.java
+++ src/main/java/servlets/Main.java
@@ -42,7 +42,5 @@ public class Main {
 
         server.join();
 
-
-
     }
 }
---
+++ src/main/java/servlets/SimpleDB.java
@@ -0,0 +1,83 @@
+package servlets;
+
+import java.sql.*;
+import java.util.Optional;
+
+/**
+ * Created by TwistedPersonality on 20.12.2015.
+ */
+public class SimpleDB
+{
+    private Statement stmt = null;
+
+    public SimpleDB(String host, String DB, String login, String pass)
+    {
+        SimpleStatement s = new SimpleStatement(host, DB, login, pass);
+        stmt = s.getStmt();
+    }
+
+    public ResultSet ExecSelect(String query)
+    {
+        try
+        {
+            return stmt.executeQuery(query);
+        }
+        catch (SQLException ex)
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            return null;
+        }
+        catch (NullPointerException ex)
+        {
+            System.out.println("NullPointerException: No connection to the database.");
+            return null;
+        }
+    }
+
+    public Optional<Integer> ExecUpdate(String query)
+    {
+        try
+        {
+            return Optional.of(stmt.executeUpdate(query));
+        }
+        catch (SQLException ex)
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            return Optional.empty();
+        }
+        catch (NullPointerException ex)
+        {
+            System.out.println("NullPointerException: No connection to the database.");
+            return Optional.empty();
+        }
+    }
+}
+
+class SimpleStatement
+{
+    private Statement stmt = null;
+
+    public SimpleStatement(String host, String DB, String user, String pass)
+    {
+        try
+        {
+            Connection conn = DriverManager.getConnection(host + '/' + DB, user, pass);
+            stmt = conn.createStatement();
+        }
+        catch (SQLException ex)
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    public Statement getStmt()
+    {
+        return  stmt;
+    }
+}
